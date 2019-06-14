ff55430dbf3674671c3f628f38d7d2b25907dfcf
Nick Freeland <nick-f@users.noreply.github.com>
2019-06-15 00:11:17 +1000

    Update now playing status icons (#2)

--- scripts/current_track.sh
+++ scripts/current_track.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
 CURRENT_TRACK_STR=$(osascript <<EOF
-if application "iTunes" is running then
   tell application "iTunes"
-    if player state is paused then
-      set playingState to "▌▌"
-    else
-      set playingState to "▶"
-    end if
-
     if player state is stopped
-      return
+      return ""
     else
+      if player state is paused then
+        set playingState to "❚❚"
+      else
+        set playingState to "▶"
+      end if
+
       return playingState & " " & name of current track & " - " & artist of current track
     end if
   end tell
-end if
 EOF)
 
 echo $CURRENT_TRACK_STR
