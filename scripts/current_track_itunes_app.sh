342dd0880f71857671eb71412cf36d5bf03b292a
Nick Freeland <nick-f@users.noreply.github.com>
2019-12-17 10:35:12 +1100

    Don't open music apps every time tmux is opened (#5)

--- scripts/current_track_itunes_app.sh
+++ scripts/current_track_itunes_app.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 CURRENT_TRACK_STR=$(osascript <<EOF
-  tell application "iTunes"
-    if player state is stopped then
-      return ""
-    else
-      if player state is paused then
-        set playingState to "❚❚"
+  if application "iTunes" is running then
+    tell application "iTunes"
+      if player state is stopped then
+        return ""
       else
-        set playingState to "▶"
-      end if
+        if player state is paused then
+          set playingState to "❚❚"
+        else
+          set playingState to "▶"
+        end if
 
-      return playingState & " " & name of current track & " - " & artist of current track
-    end if
-  end tell
+        return playingState & " " & name of current track & " - " & artist of current track
+      end if
+    end tell
+  end if
 EOF)
 
 echo $CURRENT_TRACK_STR
